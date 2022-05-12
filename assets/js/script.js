d04b5bd1f1520d394cb87fa15a7c983511d6083b
kaelajohnson <rikulova@gmail.com>
2022-05-11 18:35:09 -0600

    set a listener on Start Quiz and began testing to populate the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var quizQuestions = "Testing"
+
+
+document.getElementById("startButton").addEventListener("click", function(){
+    document.getElementById("mainText").innerHTML = quizQuestions
+
+    })
